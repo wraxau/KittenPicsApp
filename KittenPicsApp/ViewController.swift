ba3aaf783584741feead98487b81db09e2852f66
wraxau <wraxau@yandex.ru>
2026-02-06 11:26:25 +0300

    Initial Commit

---
+++ KittenPicsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KittenPicsApp
+//
+//  Created by нина on 06.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
